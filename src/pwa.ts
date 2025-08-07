130debfc9a0bcf0f2ff3fc442254e3b01e3ab4b6
katy-pel <katerina.pelouchova@gmail.com>
2025-08-08 00:49:22 +0200

    Update pwa.ts

--- src/pwa.ts
+++ src/pwa.ts
@@ -1,7 +1,7 @@
 export function registerSW() {
 if ("serviceWorker" in navigator) {
 window.addEventListener("load", () => {
-const swUrl = ${import.meta.env.BASE_URL}sw.js;
+const swUrl = `${import.meta.env.BASE_URL}sw.js`;
 navigator.serviceWorker.register(swUrl).catch(() => {});
 });
 }

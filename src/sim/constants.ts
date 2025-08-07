e658d072468c99f848ce3a3f771a05691328ab00
katy-pel <katerina.pelouchova@gmail.com>
2025-08-07 22:42:21 +0200

    Create constants.ts

---
+++ src/sim/constants.ts
@@ -0,0 +1,5 @@
+export const DT_SECONDS = 1; // délka sim kroků (1 s)
+export const RAMP_MULT = DT_SECONDS / 60;
+export const START_HOUR = 8;
+export const PUMPED_CAP_MWH = 6 * 650; // ~6h při 650 MW (Dlouhé Stráně) – didakticky
+export const PUMP_ROUNDTRIP = 0.8; // účinnost cyklu
